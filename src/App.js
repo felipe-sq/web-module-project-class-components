ba6692bf1dee8a1ca3e6c92db9921f3e76aae2c0
Felipe Quintero <ffquint@gmail.com>
2021-04-07 15:45:24 -0400

    Added sample todos objects array and constructor to store state in App component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+const todos = [
+  {
+    task: 'Organize Attic',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: '',
+    id: Date.now(),
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todos: todos,
+    }
+  }
   render() {
     return (
       <div>
