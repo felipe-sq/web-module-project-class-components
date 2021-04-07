3dcde5936bd98581101e899d05b9623f70b02f46
Felipe Quintero <ffquint@gmail.com>
2021-04-07 16:50:56 -0400

    Added code for todo, todolist and todoform. Also added code to App. I am able to add new todo items, select todos/tasks as completed and clear completed tasks. Styling has been added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './components/Todo.css'
+
 const todos = [
   {
     task: 'Organize Attic',
-    id: Date.now(),
+    id: Date.now()+1,
     completed: false
   },
   {
-    task: '',
+    task: 'Clean the oven',
     id: Date.now(),
     completed: false
   }
@@ -24,10 +28,51 @@ class App extends React.Component {
       todos: todos,
     }
   }
+
+  toggleTodo = todoId => {
+    console.log("Toggling todo", todoId);
+    const updatedTodos = this.state.todos.map(item => {
+      if (todoId === item.id) {
+        return { ...item, completed: !item.completed }
+      }
+      return item;
+    });
+    console.log("Updated todo array", updatedTodos);
+    this.setState({
+      ...this.state,
+      todos: updatedTodos
+    });
+  }
+
+  addTodo = todoName => {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          task: todoName,
+          id: Date.now(),
+          completed: false
+        }
+      ]
+    })
+  }
+
+  clearCompleted = e => {
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(item => !item.completed)
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm addTodo={this.addTodo}/>
+        </div>
+        <TodoList clearCompleted={this.clearCompleted} toggleTodo={this.toggleTodo} todos={this.state.todos} />
       </div>
     );
   }
