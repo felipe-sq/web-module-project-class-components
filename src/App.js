46b0949e1f64d3b17267aac739c62556e0a184b6
Felipe Quintero <ffquint@gmail.com>
2021-06-02 19:04:54 -0400

    Updated readme file. Imported todo components to App and added default todo items.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './components/Todo.css'
+
+const todos = [
+  {
+    task: 'Mow the lawn',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Wash laundry',
+    id: Date.now()+1,
+    completed: false
+  },
+  {
+    task: 'Do your homework!',
+    id: Date.now()+2,
+    completed: false
+  },
+  {
+    task: 'Walk the dogs',
+    id: Date.now()+3,
+    completed: false
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
